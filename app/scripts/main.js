5b208b2f652f2a81fce34e69a0e3d2e9c585cae1
Tim Kopplow <tim@arctium.org>
2016-02-11 08:51:56 +0100

    js for toggle more content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,3 +3,12 @@
   $( ".togglePerspective" ).click(function() {
     $( "main" ).toggleClass( "resetPerspective" );
   });
+
+var lessText = "Hide the resources";
+var moreText = "Show more resources";
+
+  $( ".toggleMore" ).click(function() {
+    $( ".extendedResources" ).slideToggle( "fast" );
+
+    ($(".toggleMore").text() === lessText) ? $(".toggleMore").text(moreText) : $(".toggleMore").text(lessText);
+  });
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-"use strict";$(".togglePerspective").click(function(){$("main").toggleClass("resetPerspective")});
+"use strict";$(".togglePerspective").click(function(){$("main").toggleClass("resetPerspective")});var lessText="Hide the resources",moreText="Show more resources";$(".toggleMore").click(function(){$(".extendedResources").slideToggle("fast"),$(".toggleMore").text()===lessText?$(".toggleMore").text(moreText):$(".toggleMore").text(lessText)});
