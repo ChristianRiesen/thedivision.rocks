ac54a5750d7267a1ea18f236ba040173380008b2
Tim Kopplow <tim@arctium.org>
2016-02-12 20:18:29 +0100

    overhauled the whole interface
    links are now categorized

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,3 +20,25 @@ $( "#inlineExperts" ).click(function() {
   $( ".toggleMoreThe100" ).slideToggle( "fast" );
   ($("#toggleGroups").text() === hideThe100) ? $("#toggleGroups").text(showThe100) : $("#toggleGroups").text(hideThe100);
 });
+
+$( ".officalBox .description, .officalBox h2" ).click(function() {
+  $( "#extendOffical" ).slideToggle( "fast" );
+});
+$( ".generalBox .description, .generalBox h2" ).click(function() {
+  $( "#extendGeneral" ).slideToggle( "fast" );
+});
+$( ".redditBox .description, .redditBox h2" ).click(function() {
+  $( "#extendReddit" ).slideToggle( "fast" );
+});
+$( ".sheetsBox .description, .sheetsBox h2" ).click(function() {
+  $( "#extendSheets" ).slideToggle( "fast" );
+});
+$( ".toolsBox .description, .toolsBox h2" ).click(function() {
+  $( "#extendTools" ).slideToggle( "fast" );
+});
+$( ".lfgBox .description, .lfgBox h2" ).click(function() {
+  $( "#extendLFG" ).slideToggle( "fast" );
+});
+$( ".betaBox .description, .betaBox h2" ).click(function() {
+  $( "#extendBeta" ).slideToggle( "fast" );
+});
